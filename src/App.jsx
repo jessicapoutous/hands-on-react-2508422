123db75418d19558ad20a6144e6c267326e80424
jessicapoutous <jessicapoutous@users.noreply.github.com>
2023-03-03 14:12:01 +0000

    added pics and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 function App() {
+  const name = 'StarGazers';
+  const cssFun = {
+    color:'SteelBlue',
+    fontWeight: 'bold'
+  }
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="stargazers group" />
+          <h1>Meet the <i style={cssFun}>{name}</i></h1>
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <button className="outline" onClick={()=> alert('Hi there!')}>Click Me</button>
+        </hgroup>
+      </article>
+    </div>
+    
   )
 }
 export default App
