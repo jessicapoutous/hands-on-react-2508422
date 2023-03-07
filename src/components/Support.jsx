1eacb3323f66c9a999eaa00712ce28e8b42dfb8c
jessicapoutous <jessicapoutous@users.noreply.github.com>
2023-03-07 17:26:53 +0000

    practiced using useEffect

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+const pageTitle = document.title;
 
 export default () => {
   const [count, setCount] = useState(0)
+
+  useEffect(
+    () => {
+      /*if (count > 0) {
+        document.title = `${pageTitle}--${count}`;
+      }*/
+      //display nothing if count doesn't exit and what is in the parens if it does 
+      () => {
+        count && (document.title = `${pageTitle} - ${count}`);
+      }
+    }
+  )
   return (
+    //use curly braces in return statements
     <button className="outline" onClick={() => setCount(count + 1)}>
-      {count === 0 ? "Click to support": `Supported ${count} times`}
+      {count === 0 ? "Click to support" : `Supported ${count} times`}
     </button>
   )
 }
